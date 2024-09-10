b9e31fa21161569721c12a761976575869754490
Abhinav <abhinav2011vs@gmail.com>
2024-09-10 12:52:32 +0530

    Initial Commit

---
+++ src/main/java/net/abhinav/ear/EmeraldsAndRubies.java
@@ -0,0 +1,10 @@
+package net.abhinav.ear;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EmeraldsAndRubies implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
